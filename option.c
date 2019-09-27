b2ae1536cd0815ff05a820b3602f74faab626a87
Miguel M <miguelmurca@gmail.com>
2019-09-27 10:15:42 +0100

    result_get_valid_with_data

--- option.c
+++ option.c
@@ -7,10 +7,58 @@ Result result_get_empty_valid()
     return empty_valid;
 }
 
+Result result_get_valid_with_data(void *data)
+{
+    Result data_valid;
+    data_valid.valid = true;
+    data_valid.data = data;
+    return data_valid;
+}
+
 Result result_get_invalid_reason(char *reason)
 {
     Result invalid;
     invalid.valid = false;
     invalid.reason = reason;
     return invalid;
+}
+
+Option option_none()
+{
+    Option new_option;
+    new_option.some = false;
+    new_option.data = NULL;
+    return new_option;
+}
+
+Option option_some_with_data(void *data)
+{
+    Option new_option;
+    new_option.some = true;
+    new_option.data = data;
+    return new_option;
+}
+
+Option_Int option_from_int(int data)
+{
+    Option_Int new_option;
+    new_option.some = true;
+    new_option.data = &data;
+    return new_option;
+}
+
+Option_Uint option_from_uint(unsigned int data)
+{
+    Option_Uint new_option;
+    new_option.some = true;
+    new_option.data = &data;
+    return new_option;
+}
+
+Option_Double option_from_double(double data)
+{
+    Option_Double new_option;
+    new_option.some = true;
+    new_option.data = &data;
+    return new_option;
 }
--- option.h
+++ option.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <stdbool.h>
+#include <stdlib.h>
 
 typedef struct Result
 {
@@ -9,6 +10,7 @@ typedef struct Result
 } Result;
 
 Result result_get_empty_valid();
+Result result_get_valid_with_data(void *data);
 Result result_get_invalid_reason(char *reason);
 
 typedef struct Option
@@ -16,3 +18,29 @@ typedef struct Option
     bool some;
     void *data;
 } Option;
+
+typedef struct Option_Uint
+{
+    bool some;
+    unsigned int *data;
+} Option_Uint;
+
+
+typedef struct Option_Int
+{
+    bool some;
+    int *data;
+} Option_Int;
+
+
+typedef struct Option_Double
+{
+    bool some;
+    double *data;
+} Option_Double;
+
+Option option_none();
+Option option_some_with_data(void *data);
+Option_Int option_from_int(int data);
+Option_Uint option_from_uint(unsigned int data);
+Option_Double option_from_double(double data);
