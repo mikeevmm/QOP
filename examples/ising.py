a26fd1649b00bc93010a1f72f4071994d4a60eb2
Miguel M <miguelmurca@gmail.com>
2019-10-21 09:12:06 +0100

    Made the ising example more flexible

--- examples/ising.py
+++ examples/ising.py
@@ -7,54 +7,68 @@ import random
 random.seed(8081902)
 
 layer_count = 4
-qubit_count = 3
-matrix = [[1.50000000000000,  0.000000000000000,  0.000000000000000,  0.000000000000000,
-           0.000000000000000,  0.000000000000000,  0.000000000000000,  0.000000000000000, ],
-          [0.000000000000000, -0.500000000000000,   2.00000000000000,  0.000000000000000,
-           2.00000000000000,  0.000000000000000,  0.000000000000000,  0.000000000000000, ],
-          [0.000000000000000,   2.00000000000000, -0.500000000000000,  0.000000000000000,
-           2.00000000000000,  0.000000000000000,  0.000000000000000,  0.000000000000000, ],
-          [0.000000000000000,  0.000000000000000,  0.000000000000000, -0.500000000000000,
-           0.000000000000000,   2.00000000000000,   2.00000000000000,  0.000000000000000, ],
-          [0.000000000000000,   2.00000000000000,   2.00000000000000,  0.000000000000000, -
-           0.500000000000000,  0.000000000000000,  0.000000000000000,  0.000000000000000, ],
-          [0.000000000000000,  0.000000000000000,  0.000000000000000,   2.00000000000000,
-           0.000000000000000, -0.500000000000000,   2.00000000000000,  0.000000000000000, ],
-          [0.000000000000000,  0.000000000000000,  0.000000000000000,   2.00000000000000,
-           0.000000000000000,   2.00000000000000, -0.500000000000000,  0.000000000000000, ],
-          [0.000000000000000,  0.000000000000000,  0.000000000000000,  0.000000000000000,  0.000000000000000,  0.000000000000000,  0.000000000000000,   1.50000000000000]]
-c = qop.Circuit(qubit_count)
+spin_count = 4
+optimizer_settings = {
+    'ada': {'rho': 0.95},
+    'optimize': {
+        'stop_at': 1e-4,
+        'max_iterations': 1000
+    }
+}
+
+# Build the hamiltonian
+sx = np.matrix([[0, 1], [1, 0]])
+sy = np.matrix([[0, -1.0j], [1.j, 0]])
+sz = np.matrix([[1, 0], [0, -1]])
+
+# Return the matrix for a spin of type u (i.e. x, y, z) at position j,
+# for a q qubit spin ring
+
+
+def suj(q, u, j):
+    return np.kron(np.kron(np.eye(2**j), u), np.eye(2**(q - j - 1)))
+
+
+matrix = sum(suj(spin_count, sx, i) * suj(spin_count, sx, (i + 1) % spin_count) +
+             suj(spin_count, sy, i) * suj(spin_count, sy, (i + 1) % spin_count) +
+             0.5 * suj(spin_count, sy, i) *
+             suj(spin_count, sy, (i + 1) % spin_count)
+             for i in range(spin_count))
+
+# Run the simulation
+c = qop.Circuit(spin_count)
 ry_gates = []
 for _ in range(layer_count):
     for i in range(0, 1):
-        for qubit in range(qubit_count):
+        for qubit in range(spin_count):
             ry = qop.Gate('ry', parameters=[
-                          (random.random() - 0.5) * 2 * 3.1415926])
+                (random.random() - 0.5) * 2 * 3.1415926])
             c.add_gate(ry, qubit)
             ry_gates.append(ry)
-        for qubit in range(0, qubit_count, 2):
+        for qubit in range(0, spin_count, 2):
             z = qop.Gate('z')
-            c.add_gate(z, qubit + i, (qubit + i + 1) % qubit_count)
-for qubit in range(qubit_count):
+            c.add_gate(z, qubit + i, (qubit + i + 1) % spin_count)
+for qubit in range(spin_count):
     ry = qop.Gate('ry', parameters=[(random.random() - 0.5) * 2 * 3.1415926])
     ry_gates.append(ry)
     c.add_gate(ry, qubit)
 
 start = time()
-results, broke = c.optimize(matrix, settings={'ada': {'rho': 0.90}, 'optimize': {
-    'stop_at': 1e-4}})
+results, broke = c.optimize(matrix, settings=optimizer_settings)
 end = time()
 print(results)
 print(f"in {end - start}s")
 print(("" if broke else "not ") + "breaking after max iterations")
 
 print("output:")
-out = c.run([1] + [0] * (2**qubit_count - 1))
+out = c.run([1] + [0] * (2**spin_count - 1))
 print(out)
 
 out = np.array(out, dtype=np.complex).T  # Column vector
-ham = np.array(matrix, dtype=np.complex)
 print("Out state 2-norm")
 print(np.linalg.norm(out, ord=2))
 print("Hamiltonian contraction")
-print(np.conjugate(out.T) @ ham @ out)
+print(np.conjugate(out.T) @ matrix @ out)
+
+print("Minimum eigenvalue, from numpy:")
+print(min(np.linalg.eigvals(matrix)))
